278fe91b9fb090085f50a5c569a24fb158e3784c
Freddy Heppell <frheppell1@sheffield.ac.uk>
2022-10-18 16:28:59 +0100

    Create dashboard for progress monitoring

--- setup/certs/setup.sh
+++ setup/certs/setup.sh
@@ -28,7 +28,16 @@ echo "Waiting for Elasticsearch availability";
 until curl -s --cacert config/certs/ca/ca.crt https://es01:9200 | grep -q "missing authentication credentials"; do sleep 30; done;
 
 
-
 echo "Setting kibana_system password";
 until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://es01:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
-echo "All done!";
+
+echo "Waiting for Kibana availability";
+until curl -s -u "elastic:${ELASTIC_PASSWORD}" http://kibana:5601/api/status | grep -q "All services are available"; do sleep 30; done;
+
+echo "Waiting for Metricbeat to create its dashboards";
+until curl -s -u "elastic:${ELASTIC_PASSWORD}" http://kibana:5601/api/data_views | grep -q "metricbeat-"; do sleep 30; done;
+
+echo "Importing saved objects";
+curl -s -X POST -u "elastic:${ELASTIC_PASSWORD}" http://kibana:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@saved_objects.ndjson;
+
+echo "\nAll done!";
